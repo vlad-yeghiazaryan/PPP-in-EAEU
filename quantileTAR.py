1a388446c6758213b016007e28e07e36f3abd886
Vlad-Yeghiazaryan <vladimir.y2000@gmail.com>
2021-08-03 15:40:09 +0400

    final model

--- quantileTAR.py
+++ quantileTAR.py
@@ -16,6 +16,7 @@ class QTAR:
     Parameters
     ----------
     endog   -  Nx1 matrix. Preferably pandas series or a numpy array.
+    threshold_dummy - Nx1 matrix. A series containing 1s for t>=t* (t* is the threshold date) and 0s otherwise.
     model   -   c  = Constant (Default in Koenker & Xiao (2004)).
                 ct = Constant and trend.
     pmax    -  Maximum number of lags for dy (serially correlated terms).
@@ -31,11 +32,9 @@ class QTAR:
     Lags          - the effective number of lags determined through the information criterion.
     α₀(τ)         - the magnitude of the shock.
     ρ₁(τ)         - the estimated rho parameter for a given quantile. The speed of mean reversion.
-    ρ₁(OLS)       - the estimated rho parameter for the OLS estimation.
+    α₁(τ)         - The change in the speed of mean reversion before and after the threshold.
     δ²            - A nuisance parameter used to estimate critical values. See equation (10) at page 778.
-    Half-lives    - ln(0.5)/ln(abs(ρ₁(τ))).
-    tₙ(τ)          - the quantile unit root statistic (t-ratio for a given quantile).
-    cv            - 1%, 5%, 10% critical values for the estimated δ².
+    tₙ(τ)          - the quantile unit root statistic (t-ratio for a given quantile) before the threshold.
 
     """
     def __init__(self, endog, threshold_dummy, model='c', pmax=5, ic='AIC', exog=None):
@@ -71,7 +70,6 @@ class QTAR:
                              ).add_prefix('pre-'+self.threshold_dummy.name+'_')
         y1_post = pd.DataFrame(y1.squeeze(axis=1)*np.array(self.threshold_dummy[1:])
                               ).add_prefix('post-'+self.threshold_dummy.name+'_')
-        
         dy = self.endog.diff(1)[1:]  # first difference
         X = pd.concat([y1_pre, y1_post], axis=1)
 
@@ -83,7 +81,7 @@ class QTAR:
         if p > 0:
             dyl = pd.DataFrame(lagmat(dy, maxlag=p), index=dy.index,
                                columns=['Diff.L' + str(i) for i in range(1, 1 + p)])[p:]
-            X = pd.concat([y1_pre[p:], y1_post[p:], dyl], axis=1)
+            X = pd.concat([y1[p:], y1_post[p:], dyl], axis=1)
         # If the ic decides not to include lags
         else:
             X = X[p:]
@@ -121,8 +119,7 @@ class QTAR:
         df = pd.DataFrame(results)
 
         # Adding the QKS statistic
-        df['pre-QKS'] = max(map(abs, df['pre-tₙ(τ)']))
-        df['post-QKS'] = max(map(abs, df['post-tₙ(τ)']))
+        df['QKS'] = max(map(abs, df['tₙ(τ)']))
         df['name'] = self.endog.name
         df.set_index('quantile', inplace=True)
         return df
@@ -133,18 +130,11 @@ class QTAR:
         # Running the quantile regression
         n = len(self.endog)
         qOut1 = QuantReg(self.endog, self.exog).fit(q=tau)
-        olsFit = OLS(self.endog, self.exog).fit()
         
         # calculating alpha and rho
-        alpha_tau = qOut1.params[0]
-        pre_rho_tau = qOut1.params[1]
-        post_rho_tau = qOut1.params[2]
-        pre_rho_ols = olsFit.params[1]
-        post_rho_ols = olsFit.params[2]
-
-        # Estimating Half-lifes pre_rho_tau
-        hl = np.log(0.5) / np.log(np.abs(post_rho_tau))
-        hl = '∞' if hl < 0 else round(hl, 3)
+        alpha0_tau = qOut1.params[0]
+        rho_tau = qOut1.params[1]
+        alpha1_tau = qOut1.params[2]
 
         # calculating delta2 using covariance
         ind = qOut1.resid < 0
@@ -152,9 +142,6 @@ class QTAR:
         w = self.diff
         cov = np.cov(phi, w)[0, 1]
         delta2 = (cov / (np.std(w, ddof=1) * np.sqrt(tau * (1 - tau)))) ** 2
-
-        # calculating critical values associate with our delta2
-        crv = self.crit_QRadf(delta2, self.model)
 
         # Calculating quantile bandwidth
         h = self.bandwidth(tau, n, True)
@@ -168,19 +155,19 @@ class QTAR:
             if h > (1 - tau):
                 h = (1 - tau) / 1.5
 
-        # Defining some inputs
-        if self.model == 'c':
-            X = add_constant(pd.concat([self.L1_pre, self.L1_post], axis=1))
-        elif self.model == 'ct':
-            X = add_trend(add_constant(pd.concat([self.L1_pre, self.L1_post], axis=1)), 'ct')
-        else:
-            raise ValueError(
-                'Model type is not recognized: ' + str(self.model))
-
-        X = pd.concat([X, self.extraExog], axis=1)
+        if self.lags == 0:
+            # Defining some inputs
+            if self.model == 'c':
+                X = add_constant(pd.concat([self.L1, self.L1_post], axis=1))
+            elif self.model == 'ct':
+                X = add_trend(add_constant(pd.concat([self.L1, self.L1_post], axis=1)), 'ct')
+            else:
+                raise ValueError(
+                    'Model type is not recognized: ' + str(self.model))
+            X = pd.concat([X, self.extraExog], axis=1)
 
         # The common case
-        if self.lags > 0:
+        elif self.lags > 0:
             X = self.exog
         
         # Running the other 2 QuantRegs
@@ -216,7 +203,7 @@ class QTAR:
 
         # aligning matrices
         xx = np.array(xx)
-        y1_pre = np.array(self.L1_pre)
+        y1 = np.array(self.L1)
         y1_post = np.array(self.L1_post)
 
         # Constructing a NxN matrix
@@ -224,49 +211,30 @@ class QTAR:
             xx.dot(np.linalg.inv(np.dot(xx.T, xx))).dot(xx.T)
         fzCrt = fz / np.sqrt(tau * (1 - tau))
         
-        # We will preform two unit root test for the series before 
-        # and after the threshold
-        eqPX_pre = np.sqrt((y1_pre.T.dot(PX).dot(y1_pre))[0][0])
-        eqPX_post = np.sqrt((y1_post.T.dot(PX).dot(y1_post))[0][0])
+        # We will preform a single unit root test for the series before the threshold
+        eqPX = np.sqrt((y1.T.dot(PX).dot(y1))[0][0])
 
-        # QADF statistics for y1_pre and y1_post
-        QURtadf_pre = fzCrt * eqPX_pre * (pre_rho_tau - 1)
-        QURtadf_post = fzCrt * eqPX_post * (post_rho_tau - 1)
-        cv = self.crit_QRadf(delta2, self.model)
+        # QADF statistic
+        QURadf = fzCrt * eqPX * (rho_tau - 1)
 
         # Exposing variables
         self.tau = tau
-        self.alpha_tau = alpha_tau
-        self.pre_rho_tau = pre_rho_tau
-        self.post_rho_tau = post_rho_tau
-        self.pre_rho_ols = pre_rho_ols
-        self.post_rho_ols = post_rho_ols
+        self.alpha0_tau = alpha0_tau
+        self.rho_tau = rho_tau
+        self.alpha1_tau = alpha1_tau
         self.delta2 = delta2
-        self.QURtadf_pre = QURtadf_pre
-        self.QURtadf_post = QURtadf_post
-        self.hl = hl
+        self.QURadf = QURadf
         self.regression = qOut1
-        self.cvs = {
-            'CV10%': cv['10%'],
-            'CV5%': cv['5%'],
-            'CV1%': cv['1%']
-        }
+        
+        # Exposing final results
         self.results = {
             'quantile': round(self.tau, 2),
             'Lags': self.lags,
-            'α\u2080(τ)': round(self.alpha_tau, 4),
-            'pre-ρ\u2081(τ)': round(self.pre_rho_tau, 4),
-            'post-ρ\u2082(τ)': round(self.post_rho_tau, 4),
-            'pre-ρ\u2081(OLS)': round(self.pre_rho_ols, 4),
-            'post-ρ\u2082(OLS)': round(self.post_rho_ols, 4),
-            'ρ\u2081(τ)-ρ\u2082(τ)':round(self.pre_rho_tau-self.post_rho_tau, 5),
+            'α\u2080(τ)': round(self.alpha0_tau, 4),
+            'ρ\u2081(τ)': round(self.rho_tau, 4),
+            'α\u2081(τ)': round(self.alpha1_tau, 5),
             'δ\u00B2': round(self.delta2, 3),
-            'Half-lives': self.hl,
-            'pre-t\u2099(τ)': round(self.QURtadf_pre, 4),
-            'post-t\u2099(τ)': round(self.QURtadf_post, 4),
-            'CV10%': round(self.cvs['CV10%'], 4),
-            'CV5%': round(self.cvs['CV5%'], 4),
-            'CV1%': round(self.cvs['CV1%'], 4)
+            't\u2099(τ)': round(self.QURadf, 4)
         }
         return self
 
@@ -286,64 +254,6 @@ class QTAR:
             h = n**(-0.2) * ((4.5 * f0**4) / (2 * x0**2 + 1)**2)**0.2
 
         return h
-
-    @staticmethod
-    @numba.jit(forceobj=True, parallel=True)
-    def crit_QRadf(r2, model):
-        ncCV = [[-2.4611512, -1.783209, -1.4189957],
-                [-2.494341, -1.8184897, -1.4589747],
-                [-2.5152783, -1.8516957, -1.5071775],
-                [-2.5509773, -1.895772, -1.5323511],
-                [-2.5520784, -1.8949965, -1.541883],
-                [-2.5490848, -1.8981677, -1.5625462],
-                [-2.5547456, -1.934318, -1.5889045],
-                [-2.5761273, -1.9387996, -1.602021],
-                [-2.5511921, -1.9328373, -1.612821],
-                [-2.5658, -1.9393, -1.6156]]
-
-        cCV = [[-2.7844267, -2.115829, -1.7525193],
-               [-2.9138762, -2.2790427, -1.9172046],
-               [-3.0628184, -2.3994711, -2.057307],
-               [-3.1376157, -2.5070473, -2.168052],
-               [-3.191466, -2.5841611, -2.2520173],
-               [-3.2437157, -2.639956, -2.316327],
-               [-3.2951006, -2.7180169, -2.408564],
-               [-3.3627161, -2.7536756, -2.4577709],
-               [-3.3896556, -2.8074982, -2.5037759],
-               [-3.4336, -2.8621, -2.5671]]
-
-        ctCV = [[-2.9657928, -2.3081543, -1.9519926],
-                [-3.1929596, -2.5482619, -2.1991651],
-                [-3.3727717, -2.7283918, -2.3806008],
-                [-3.4904849, -2.8669056, -2.5315918],
-                [-3.6003166, -2.9853079, -2.6672416],
-                [-3.6819803, -3.095476, -2.7815263],
-                [-3.7551759, -3.178355, -2.8728146],
-                [-3.8348596, -3.2674954, -2.973555],
-                [-3.8800989, -3.3316415, -3.0364171],
-                [-3.9638, -3.4126, -3.1279]]
-
-        # Selecting the critical values set based on model type
-        cvs = {'nc': ncCV, 'c': cCV, 'ct': ctCV}
-        cv = cvs[model]
-
-        delta2 = pd.Series(np.arange(0.1, 1.1, 0.1), name='delta2')
-        crt = pd.DataFrame(cv, index=delta2, columns=['1%', '5%', '10%'])
-
-        if r2 < 0.1:
-            ct = crt.iloc[0, :]
-        else:
-            r210 = r2 * 10
-            if (r210) >= 10:
-                ct = crt.iloc[9, :]
-            else:
-                #  Main logic goes here
-                r2a = int(np.floor(r210))
-                r2b = int(np.ceil(r210))
-                wa = r2b - r210
-                ct = wa * crt.iloc[(r2a - 1), :] + (1 - wa) * \
-                    crt.iloc[(r2b - 1), :]
-        return ct
 
     def __repr__(self):
         try:
@@ -393,18 +303,13 @@ def addStars(row, test, significanceLevels, rounding=2):
     return starValue
 
 def QTAR_CustomRport(CountryQADF, results, significanceLevels, dropColumns=None):
-    values =  ['α₀(τ)', 'pre-ρ₁(τ)', 'post-ρ₂(τ)', 'pre-ρ₁(OLS)', 'post-ρ₂(OLS)', 
-               'pre-tₙ(τ)', 'post-tₙ(τ)', 'ρ₁(τ)-ρ₂(τ)']
+    values =  ['α₀(τ)', 'ρ₁(τ)', 'α₁(τ)', 'tₙ(τ)']
     for value in values:
         test = twoTail if value == 'α₀(τ)' else oneTailLower
-        rounding = 2 if value != 'ρ₁(τ)-ρ₂(τ)' else 4
+        rounding = 2 if value != 'α₁(τ)' else 4
         CountryQADF[value] = pd.concat([CountryQADF, results.groupby('quantile')[value].apply(lambda x:np.array(x))], axis=1)[value].apply(addStars, axis=1, args=[test, significanceLevels, rounding])
-    CountryQADF['pre-QKS'] = addStars([CountryQADF['pre-QKS'].mean(), results.groupby('name')['pre-QKS'].mean()], oneTailUpper, significanceLevels)
-    CountryQADF.loc[0.2:, 'pre-QKS'] = ''
-    CountryQADF['post-QKS'] = addStars([CountryQADF['post-QKS'].mean(), results.groupby('name')['post-QKS'].mean()], oneTailUpper, significanceLevels)
-    CountryQADF.loc[0.2:, 'post-QKS'] = ''
-    CountryQADF['Half-lives'] = CountryQADF['Half-lives'].apply(lambda x: round(x/12) if not isinstance(x, str) else x)
-    CountryQADF.rename(columns={'Half-lives':'Half-lives (years)'}, inplace=True)
+    CountryQADF['QKS'] = addStars([CountryQADF['QKS'].mean(), results.groupby('name')['QKS'].mean()], oneTailUpper, significanceLevels)
+    CountryQADF.loc[0.2:, 'QKS'] = ''
     CountryQADF.drop(columns=dropColumns, inplace=True) if dropColumns != None else None
 
     # final cleanup
